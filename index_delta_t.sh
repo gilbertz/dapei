281e91093104cfb3a517dae8e3d8b0034d1a03dc
root <root@iZ28bm44n2pZ.(none)>
2014-12-03 16:17:11 +0800

    add index script

---
+++ index_delta_t.sh
@@ -0,0 +1,5 @@
+ /usr/local/coreseek/bin/indexer -c /data/dapeimishu/public/sphinx_mysql.conf --rotate dapei_delta > /tmp/sjb.sphinx.dapei_delta.log
+ /usr/local/coreseek/bin/indexer -c /data/dapeimishu/public/sphinx_mysql.conf --rotate matter_delta > /tmp/sjb.sphinx.matter_delta.log
+ /usr/local/coreseek/bin/indexer -c /data/dapeimishu/public/sphinx_mysql.conf --rotate brand_delta > /tmp/sjb.sphinx.brand_delta.log
+  /usr/local/coreseek/bin/indexer -c /data/dapeimishu/public/sphinx_mysql.conf --rotate user_delta > /tmp/sjb.sphinx.user_delta.log
+
---
+++ index_main_t.sh
@@ -0,0 +1,1 @@
+/usr/local/coreseek/bin/indexer -c /data/dapeimishu/public/sphinx_mysql.conf --rotate --all > /tmp/sjb.sphinx.all.log
