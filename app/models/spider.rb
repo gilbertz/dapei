36084c33e7923b22084571410e50a892c3d16129
root <root@AY140521225939574bc7Z.(none)>
2014-12-21 13:02:17 +0800

    dd

--- app/models/spider.rb
+++ app/models/spider.rb
@@ -8,6 +8,43 @@ class Spider < ActiveRecord::Base
     SpiderPages.where(:spider_id => self.id)
   end
 
+
+  def get_docs
+    doc_dict = {}
+
+    spider_pages = self.get_spider_pages
+    spider_pages.each do |c|
+      page_id = c.id
+      pkey = "brand_#{brand_id}_spider_#{spider_id}_spider_pages_#{page_id}"
+      lks = @redis.lrange(pkey, 0, -1)
+      brand_cat_list_length = lks.length
+      p pkey,lks.length
+
+      next if brand_cat_list_length == 0
+      
+      docs = []
+      lks.each do |link|
+        p link
+        next if link.nil?
+        link_docid = make_docid(link)
+        doc_yml = @redis.get link_docid
+        next if doc_yml.nil?
+        doc = YAML::load( doc_yml )
+        doc['docid'] = link_docid
+        doc['link'] = link
+        doc['category_id'] = c.parent_id
+        u = User.create_by_brand(brand_id)
+        doc['user_id'] = u.id
+        doc['page_id'] = page_id
+        doc['sub_category_id'] = c.get_category(u.id).id
+        docs << doc
+      end
+      doc_dict[page_id] = docs
+    end
+    doc_dict
+  end
+
+
   def invert_state
     if self.stop
       self.stop = false
