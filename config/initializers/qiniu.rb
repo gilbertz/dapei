52ba20f81510dcb1c1e3a84e6687c7c7fb271a49
root <root@iZ28bm44n2pZ.(none)>
2015-02-05 23:11:32 +0800

    add qiniu config

---
+++ config/initializers/qiniu.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+require 'qiniu'
+
+Qiniu.establish_connection! :access_key => 'WwVvV8g0SsN_HzpFfEHD3dwUL0YIjYZWQOuLqkGL',
+                            :secret_key => '6bj94Ey-s1a9t0c2Eu4Mx5y8guht6py78KktGoSW'
